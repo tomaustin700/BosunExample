c82d200f79dd52dc9638d22ea1b9088716c79856
tomaustin700 <tomaustin700@gmail.com>
2018-12-09 20:47:39 +0000

    Tidied things up a bit

--- BosunTest/Program.cs
+++ BosunTest/Program.cs
@@ -5,44 +5,67 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace BosunTest
 {
     class Program
     {
-        static HttpClient client = new HttpClient();
-        static MetricsCollector collector = new MetricsCollector(new BosunOptions(ex => Handle(ex))
-        {
-            MetricsNamePrefix = "TestApp.",
-            BosunUrl = new System.Uri("http://192.168.1.5:8070"),
-            PropertyToTagName = NameTransformers.CamelToLowerSnakeCase,
-            ThrowOnPostFail = true,
-            DefaultTags = new Dictionary<string, string>
-        { {"host", NameTransformers.Sanitize(Environment.MachineName.ToLower())} }
-        });
-        static EventGauge timer = collector.CreateMetric<EventGauge>("GetWeather", "time taken", "measures time taken to get weather from api");
+        static HttpClient _client = new HttpClient();
+        static MetricsCollector _collector;
+        static EventGauge _timer;
 
         static void Main(string[] args)
         {
 
-            RunAsync().GetAwaiter().GetResult();
+            _collector = new MetricsCollector(new BosunOptions(ex => Handle(ex))
+            {
+                MetricsNamePrefix = "TestApp.",
+                BosunUrl = new System.Uri("http://192.168.1.5:8070"),
+                PropertyToTagName = NameTransformers.CamelToLowerSnakeCase,
+                ThrowOnPostFail = true,
+                DefaultTags = new Dictionary<string, string>
+        { {"host", NameTransformers.Sanitize(Environment.MachineName.ToLower())} }
+            });
+
+            _timer = _collector.CreateMetric<EventGauge>("GetWeather", "time taken", "measures time taken to get weather from api");
+
+            var dispatcherTimer = new Timer();
+            dispatcherTimer.Interval = 5000;
+            dispatcherTimer.Start();
+            dispatcherTimer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
 
             Console.ReadLine();
         }
 
-        static async Task RunAsync()
-        {
 
-            await GetWeather();
+
+        static async void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            await Time(async () =>
+            {
+                await GetWeather();
+            }, _timer);
+
         }
 
         static async Task GetWeather()
         {
+
+            HttpResponseMessage response = await _client.GetAsync("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+
+            Console.WriteLine("Got Weather");
+        }
+
+        public static async Task Time(Func<Task> action, EventGauge gauge)
+        {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            HttpResponseMessage response = await client.GetAsync("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+            await action.Invoke();
             stopwatch.Stop();
-            timer.Record(stopwatch.Elapsed.TotalMilliseconds, DateTime.Now);
+            gauge.Record(stopwatch.Elapsed.TotalMilliseconds, DateTime.Now);
+
+
         }
 
         static void Handle(Exception ex)
